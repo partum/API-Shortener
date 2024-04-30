de96f19e7705b148f0fa0ef6c6758295ae9483a5
Julia Arrington <jarring.dev@gmail.com>
2024-04-30 10:42:31 -0600

    setup

---
+++ src/components/header.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+export default function Header(props) {
+    <div>
+        <h2>test</h2>
+        <img/>
+        <a href="#">Features</a>
+        <a href="#">Pricing</a>
+        <a href="#">Resources</a>
+        <a href="#">Login</a>
+        {/* <roundButton text="Sign Up" size="small"/> */}
+    </div>
+}
