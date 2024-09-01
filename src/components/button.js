775d3a7d068b6dbf16ea0717c6fdf3c1b7fd275a
Julia Arrington <jarring.dev@gmail.com>
2024-08-31 18:19:40 -0600

    created button component

---
+++ src/components/button.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default function Button(props) {
+    return (
+        <a href="partum.github.io" className='button'>
+            {props.text}
+        </a>
+    )
+}
