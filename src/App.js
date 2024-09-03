0f53ac1ce0e1cfade94ea6a306f2f9b73a4d4d2c
Julia Arrington <jarring.dev@gmail.com>
2024-09-03 13:58:41 -0600

    moved css file. Created hero component

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import './styles/App.css';
 import Nav from './components/nav.js'
 import Hero from './components/hero.js'
 import Stats from './components/stats.js'
+import Cta from './components/cta.js'
 
 
 function App() {
@@ -15,6 +16,7 @@ function App() {
       <div className='stats'>
         <Stats/>
       </div>
+      <Cta/>
       <div class="attribution">
         Challenge by <a href="https://www.frontendmentor.io?ref=challenge" target="_blank" rel="noreferrer">Frontend Mentor</a>. 
         Coded by <a href="https://partum.github.io" target="_blank" rel="noreferrer">Julia Arrington</a>.
---
+++ src/components/hero.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import Button from './button.js';
+
+export default function Hero() {
+    return (
+        <div className='hero'>
+      <div class="container">
+        <div className='container-h1'>
+            <h1>More than just shorter links</h1>
+            <p>Build your brand’s recognition and get detailed insights 
+            on how your links are performing.</p>
+            <Button text="Get Started"/>
+        </div>
+      </div>
+      </div>
+    )
+}
+
+
