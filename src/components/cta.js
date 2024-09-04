91daaca01599ee74551598bf34ed2868b3e7cdf8
Julia Arrington <jarring.dev@gmail.com>
2024-09-04 15:27:21 -0600

    added CTA section

---
+++ src/components/cta.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import Button from './button.js';
+
+export default function Cta() {
+    return (
+        <div className='cta'>
+            <div>
+                <h2>Boost your links today</h2>
+                <Button text='Get Started'/>
+                <div className='spacer-20'></div>
+            </div>
+        </div>
+    )
+}
+
+
