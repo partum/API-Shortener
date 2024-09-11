17c1416d1232abea3e7c6d873c06f1b246d831fc
Julia Arrington <jarring.dev@gmail.com>
2024-09-11 16:03:47 -0600

    created bar component

---
+++ src/components/bar.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+export default function bar() {
+    return (
+        <div class="container bar">
+            <form action="">
+                <input type="text" id="link" value="Shorten a link here..."/>
+                <input type="submit" value="Shorten It!" class="button"/>
+            </form> 
+        </div>
+    )
+}
+
+
